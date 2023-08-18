c4698754dbb580c27abdb6371876ba0ad3481bc8
daniel <danielmilewski123@gmail.com>
2023-08-18 14:47:06 +0200

    Create loadAccount, loadContract and render it to show on website

--- src/app.js
+++ src/app.js
@@ -11,33 +11,26 @@ App = {
 
 	// https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
 	loadWeb3: async () => {
-		console.log('loadWeb3')
 		if (typeof web3 !== 'undefined') {
-			console.log('1')
 			App.web3Provider = web3.currentProvider
 			web3 = new Web3(web3.currentProvider)
 		} else {
-			console.log('2')
 			window.alert("Please connect to Metamask.")
 		}
 		// Modern dapp browsers...
 		if (window.ethereum) {
-			console.log('3')
 			window.web3 = new Web3(ethereum)
 			try {
-				console.log('4 Request account access if needed')
 				// Request account access if needed
 				await ethereum.enable()
 				// Acccounts now exposed`
 				web3.eth.sendTransaction({/* ... */})
 			} catch (error) {
-				console.log('5 User denied account access...')
 				// User denied account access...
 			}
 		}
 		// Legacy dapp browsers...
 		else if (window.web3) {
-			console.log('6')
 			App.web3Provider = web3.currentProvider
 			window.web3 = new Web3(web3.currentProvider)
 			// Acccounts always exposed
@@ -45,7 +38,6 @@ App = {
 		}
 		// Non-dapp browsers...
 		else {
-			console.log('7')
 			console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
 		}
 	},
@@ -57,27 +49,15 @@ App = {
     console.log("Current account:", App.account);
 	},
 
-	// loadContract: async () => {
-	// 	// Create a JavaScript version of the smart contract
-	// 	const todoList = await $.getJSON('TodoList.json');
-	// 	App.contracts.TodoList = artifacts.require('TodoList');
-	// 	App.contracts.TodoList.setProvider(App.web3Provider)
-	// 	console.log('todoList', todoList);
-	// },
-
 	loadContract: async () => {
-    // Create a JavaScript version of the smart contract
-    const todoList = await $.getJSON('TodoList.json');
-    const web3Provider = App.web3Provider;
-
-    // Get ABI from todoList
-    const contractABI = todoList.abi;
-    const contractAddress = todoList.networks[5777].address;
-
-    // Create contract instance
-    App.contracts.TodoList = new web3.eth.Contract(contractABI, contractAddress);
-
-    console.log('todoList', todoList);
+	  // Create a JavaScript version of the smart contract
+	  const todoList = await $.getJSON('TodoList.json')
+	  console.log("todoList", todoList)
+	  App.contracts.TodoList = await TruffleContract(todoList)
+	  App.contracts.TodoList.setProvider(App.web3Provider)
+  
+	  // Hydrate the smart contract with values from the blockchain
+	  App.todoList = await App.contracts.TodoList.deployed()
 	},
 
 	render: async () => {
@@ -92,16 +72,45 @@ App = {
 		// Render Account
 		$('#account').html(App.account)
 
+		// Render Tasks
+		await App.renderTasks()
+
 		// Update loading state
 		App.setLoading(false)
 	},
 
 	renderTasks: async () => {
 		// Load the total task count from the blockchain
+		console.log("App.todoList", App.todoList)
+		const taskCount = await App.todoList.taskCount()
+		const $taskTemplate = $('.taskTemplate')
 
 		// Render out each task with a new task template
+		for (var i = 1; i <= taskCount; i++) {
+			const task = await App.todoList.tasks(i)
+			const taskId = task[0].toNumber()
+			const taskContent = task[1]
+			const taskCompleted = task[2]
 
-		// Show the task
+			// Create the html for the task
+			const $newTaskTemplate = $taskTemplate.clone()
+			$newTaskTemplate.find('.content').html(taskContent)
+			$newTaskTemplate.find('input')
+							.prop('name', taskId)
+							.prop('checked', taskCompleted)
+							.on('click', App.toggleCompleted)
+
+			// Put the task in the correct list
+			if (taskCompleted) {
+				$('#completedTaskList').append($newTaskTemplate)
+			} else {
+				$('#taskList').append($newTaskTemplate)
+			}
+		
+			// Show the task
+			$newTaskTemplate.show()
+
+		}
 	},
 
 	setLoading: (boolean) => {
